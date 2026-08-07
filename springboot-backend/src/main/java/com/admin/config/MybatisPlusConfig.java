39473c695e2625e1f2774af2cbd814ca7216b2be
flux-panel-2dev <dev@local>
2026-08-07 18:40:01 +0100

    chore: 更新 MybatisPlusConfig 注释为 PostgreSQL

--- springboot-backend/src/main/java/com/admin/config/MybatisPlusConfig.java
+++ springboot-backend/src/main/java/com/admin/config/MybatisPlusConfig.java
@@ -16,7 +16,7 @@ public class MybatisPlusConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        // 配置分页插件，指定数据库类型为 SQLite
+        // 配置分页插件，指定数据库类型为 PostgreSQL
         PaginationInnerInterceptor paginationInterceptor = new PaginationInnerInterceptor(DbType.POSTGRE_SQL);
         interceptor.addInnerInterceptor(paginationInterceptor); // 分页插件
         interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor()); // 防止全表更新插件
